e620ba77e2b41652ee0f55e7120ad3ec551ce5b8
Priyanshu Kumar <priyanshu.511.kr@gmail.com>
2025-06-30 20:28:36 +0530

    add router pages scripts

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 
+const router = require('./router/PersonRoutes');
+
 require('dotenv').config();
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','./views')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(router);
 
 const port = 3000 || process.env.PORT;
 
